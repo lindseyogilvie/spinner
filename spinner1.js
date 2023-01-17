56a3cfa3e48d9b3e9174a1cc1cadc8d22ee9c2a3
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-01-17 14:08:57 -0800

    Using \r to animate console log results by printing in the same spot

---
+++ spinner1.js
@@ -0,0 +1,37 @@
+delay = 100
+
+setTimeout(() => {
+  process.stdout.write('\r| ');
+}, delay);
+
+setTimeout(() => {
+  process.stdout.write('\r/ ');
+}, delay += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r- ');
+}, delay += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r\\ ');
+}, delay += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r| ');
+}, delay += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r/ ');
+}, delay += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r- ');
+}, delay += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r\\ ');
+}, delay += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r| ');
+}, delay += 200);
